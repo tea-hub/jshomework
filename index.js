139f5c6853858526d3ab116f2997202f0c48a664
tea-hub <rostiashvilitika@gmail.com>
2020-11-09 14:52:53 +0400

    jshomework

---
+++ index.js
@@ -0,0 +1,35 @@
+let admin
+
+let name
+
+name='tea';
+
+admin='name';
+
+console.log(name)
+console.log(admin)
+
+let name='david'
+console.log('hello $(1)'); //?
+console.log('hello $["name"]'); //?
+console.log('hello $[name]'); //?
+
+let names='50'
+
+if (names==40) {
+    alert('not enough')
+} else if (names==55) {
+    console.log('more')
+} else if (names==50) {
+    console.log('names')
+} else { 
+console.log('error')
+}  
+
+const birthday='17.07.1999';
+
+if (2020-birthday=18) {
+    console.log('adult')
+} if else (2020-birthday<18) {
+    console.log=('not adult')
+}
